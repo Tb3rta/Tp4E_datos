ea6012577a52430e4610fac0507513b2529a5d15
Tb3rta <tomasberta18@gmail.com>
2022-10-25 23:17:33 -0300

    commit subiendo

--- Ejercicio1_2/LinkedBinaryTreeExt.py
+++ Ejercicio1_2/LinkedBinaryTreeExt.py
@@ -1,9 +1,10 @@
+from typing import Any, List
 from Ejercicio1_2.LinkedBinaryTreeExtAbstract import LinkedBinaryTreeExtAbstract
 from python_ed_fcad_uner.data_structures import LinkedBinaryTree, BinaryTreeNode
 
 
 class LinkedBinaryTreeExt(LinkedBinaryTree,LinkedBinaryTreeExtAbstract):
-    def hermanos(self, nodo1 : BinaryTreeNode, nodo2: BinaryTreeNode) -> bool: 
+    def hermanos(self, nodo1 : BinaryTreeNode, nodo2: BinaryTreeNode) -> bool: # si son hijo izquierdo e hijo derecho del mismo nodo padre
         pass
 
     def hojas(self) -> List[Any]:
@@ -15,4 +16,3 @@ class LinkedBinaryTreeExt(LinkedBinaryTree,LinkedBinaryTreeExtAbstract):
     def altura(self, nodo : BinaryTreeNode) -> int:
         pass
 
-    
