462d74c6a13d2d99dbf7d2efe0c41a70c2daac14
Tb3rta <tomasberta18@gmail.com>
2022-10-25 23:34:55 -0300

    commit nuevo

--- Ejercicio1_2/LinkedBinaryTreeExt.py
+++ Ejercicio1_2/LinkedBinaryTreeExt.py
@@ -5,9 +5,18 @@ from python_ed_fcad_uner.data_structures import LinkedBinaryTree, BinaryTreeNode
 
 class LinkedBinaryTreeExt(LinkedBinaryTree,LinkedBinaryTreeExtAbstract):
     def hermanos(self, nodo1 : BinaryTreeNode, nodo2: BinaryTreeNode) -> bool: # si son hijo izquierdo e hijo derecho del mismo nodo padre
+        #Si el nodo que vamos a insertar nodo1 cual es su padre?
+        #nodo 2 cual es su padre? 
+        #si los padres son == entonces 👌 hermanos
+        # si son equals? no son hermanos.
+
+        
         pass
 
     def hojas(self) -> List[Any]:
+        # un nodo es hoja cuando no tiene hijos. no es padre
+        #
+
         pass
     def internos(self) -> List[Any]:
         pass
