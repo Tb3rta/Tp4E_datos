fac2576ce802b9d7b77168f9dd8eb3315220600d
Tb3rta <tomasberta18@gmail.com>
2022-10-25 23:13:23 -0300

    commit probando

--- Ejercicio1_2/LinkedBinaryTreeExt.py
+++ Ejercicio1_2/LinkedBinaryTreeExt.py
@@ -1,4 +1,18 @@
+from Ejercicio1_2.LinkedBinaryTreeExtAbstract import LinkedBinaryTreeExtAbstract
+from python_ed_fcad_uner.data_structures import LinkedBinaryTree, BinaryTreeNode
 
 
-class LinkedBinaryTreeExt(LinkedBinaryTree):
-    pass
+class LinkedBinaryTreeExt(LinkedBinaryTree,LinkedBinaryTreeExtAbstract):
+    def hermanos(self, nodo1 : BinaryTreeNode, nodo2: BinaryTreeNode) -> bool: 
+        pass
+
+    def hojas(self) -> List[Any]:
+        pass
+    def internos(self) -> List[Any]:
+        pass
+    def profundidad(self, nodo : BinaryTreeNode) -> int:
+        pass
+    def altura(self, nodo : BinaryTreeNode) -> int:
+        pass
+
+    
